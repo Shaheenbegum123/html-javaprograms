a3f6fda4df3939aab6505104002bb12298ebba24
Shaheenbegum123 <shaheen98491@gmail.com>
2025-07-01 12:24:48 +0530

    Add files via upload

---
+++ javascript/javascript/javascript/conditional.js
@@ -0,0 +1,11 @@
+let age=18;
+if(age>18){
+    console.log("you are eligible to vote.");
+}
+else if(age<18){
+    console.log("you are not eligible to vote.");
+
+}
+else if (age==18){
+    console.log("you are exactly 18 to vote.");
+}
---
+++ javascript/javascript/javascript/datatypes.js
@@ -0,0 +1,26 @@
+let a=10; //int//
+let b=10.78;//float//
+let c="string"; //string//
+let d=true; //boolean//
+let e=null; //null//
+let f=undefined; //undefined//
+let g;           //undefined//
+let h=Symbol("symbol");   //symbol//
+let i=BigInt(12345678901234567890n); //bigint//
+let s={
+    name: "jhon",    //disctionary[key value pairs]//
+    age:30,
+    isStudent: false
+}
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
+console.log(i);
+console.log(s);
+
+
